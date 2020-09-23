55fd969ab2eec9623d95b1da2e63801ecb4abddf
Guilherme Moraes <guilhermemoraes@MacBook-Air-de-Guilherme.local>
2020-09-23 18:16:07 -0300

    Criado projeto que irá receber informações do front e ser o produtor para a fila

---
+++ back/src/test/java/br/com/fiap/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
